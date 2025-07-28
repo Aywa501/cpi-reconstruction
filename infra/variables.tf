58b822a3a56d652f14783482e2a4ffec3be72c3c
Alexander <71293864+Aywa501@users.noreply.github.com>
2025-07-27 21:43:51 -0700

    feat(step0): add Terraform variables

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "billing_account" {
+  description = "GCP billing account ID"
+  type        = string
+}
